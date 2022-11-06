fe2e9c322b713034800cf873ed2d5e6e04bc4a99
Kevin McPhillips <github@kevinmcphillips.ca>
2022-11-05 21:35:32 -0400

    Add a web endpoint

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ ruby "3.0.4"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "activesupport"
+gem "sinatra"
+gem "webrick"
--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 require "active_support/all"
+require "webrick"
+require "sinatra"
 require "fileutils"
 
 puts "app started..."
@@ -8,8 +10,18 @@ test_file = Pathname.new(File.dirname(__FILE__)).expand_path.join("running.txt")
 puts "creating test file #{test_file}..."
 FileUtils.touch(test_file)
 
-puts "ticking..."
-loop do
-  puts "  tick #{ Time.now.to_i } #{ Time.current }"
-  sleep 1
+Thread.new do
+  puts "ticking..."
+  loop do
+    puts "  tick #{ Time.now.to_i } #{ Time.current }"
+    sleep 1
+  end
 end
+
+puts "launching web app..."
+class WebApp < Sinatra::Application
+  get "/" do
+    "Hello! #{ Time.now.to_i } #{ Time.current }"
+  end
+end
+WebApp.run!
