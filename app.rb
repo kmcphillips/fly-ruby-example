2bf1e999c018827d668a162478e8a04f3d59fcba
Kevin McPhillips <github@kevinmcphillips.ca>
2022-11-06 14:12:01 -0500

    Connect to a MySQL at DATABASE_URL

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 require "active_support/all"
+require "mysql"
 require "webrick"
 require "sinatra"
 require "fileutils"
+
+Global = Class.new do
+  attr_accessor :mysql_version
+end.new
 
 puts "app started..."
 
 test_file = Pathname.new(File.dirname(__FILE__)).expand_path.join("running.txt")
 puts "creating test file #{test_file}..."
 FileUtils.touch(test_file)
+
+db = Mysql.connect(ENV["DATABASE_URL"])
+db.query("SHOW VARIABLES LIKE 'version'").each do |(name, value)|
+  Global.mysql_version = value
+end
 
 Thread.new do
   puts "ticking..."
@@ -21,7 +31,7 @@ end
 puts "launching web app..."
 class WebApp < Sinatra::Application
   get "/" do
-    "Hello! #{ Time.now.to_i } #{ Time.current }"
+    "MySQL #{ Global.mysql_version } #{ Time.now.to_i } #{ Time.current }"
   end
 end
 WebApp.run!
